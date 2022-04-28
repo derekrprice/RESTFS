9f0c0a3d0c175b4251a0ae64d343e8d3fac59a12
Derek Price <derekrobertprice@gmail.com>
2022-04-28 02:11:30 -0400

    Use singular names for models.  Document models.

--- FSAPI/admin.py
+++ FSAPI/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Topic
+from .models import Folder
+from .models import Document
 
-# Register your models here.
+admin.site.register(Topic)
+admin.site.register(Folder)
+admin.site.register(Document)
--- FSAPI/models.py
+++ FSAPI/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Topics(models.Model):
+class Topic(models.Model):
     name = models.CharField(max_length=15,unique=True)
     description = models.CharField(max_length=255)
 
@@ -11,28 +11,28 @@ class Topics(models.Model):
 
 class INode(models.Model):
     name = models.CharField(max_length=255)
-    parent = models.ForeignKey('Folders', on_delete=models.CASCADE)
-    topics = models.ManyToManyField(Topics)
+    parent = models.ForeignKey('Folder', on_delete=models.CASCADE)
+    topics = models.ManyToManyField(Topic, blank=True)
 
     class Meta:
-        abstract = True
         constraints = [models.UniqueConstraint(
             fields=['parent', 'name'],
             name='%(app_label)s_%(class)s_unique_path')
         ]
+        db_table = 'INodes'
         indexes = [models.Index(fields=['parent', 'name'])]
         ordering = ['name']
 
 
-class Folders(INode):
+class Folder(INode):
 
-    class Meta(INode.Meta):
+    class Meta:
         db_table = 'Folders'
 
 
-class Documents(INode):
+class Document(INode):
     content = models.CharField(max_length=4096)
 
-    class Meta(INode.Meta):
+    class Meta:
         db_table = 'Documents'
 
