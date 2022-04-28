6558480002e6958cc627ac1e4ee5c9518ec40074
Derek Price <derekrobertprice@gmail.com>
2022-04-28 04:26:39 -0400

    Add support for heroku.

--- RESTFS/settings.py
+++ RESTFS/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m4@wp8k-dmsuegw$x_an4o_s_*4oyr8hyg7&m0&njrl5$0)vf7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
